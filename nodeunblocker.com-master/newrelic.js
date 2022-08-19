6dca74f2904f15f77621a5d0c83f92931cdafdd8
Kira <95455955+ZemTwo@users.noreply.github.com>
2022-08-19 09:41:56 -0600

    Update newrelic.js

--- nodeunblocker.com-master/newrelic.js
+++ nodeunblocker.com-master/newrelic.js
@@ -8,7 +8,7 @@ exports.config = {
     /**
      * Array of application names.
      */
-    app_name: ['Node Unblocker'],
+    app_name: ['Santa Cruz'],
     /**
      * Your New Relic license key.
      */
